cb321e189db95ce421523d9603e60c0e0b973393
chung <wjdac66@gmail.com>
2023-02-01 18:04:02 +0900

    login test

--- src/main/java/com/web_service/notice_board/web/IndexController.java
+++ src/main/java/com/web_service/notice_board/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.web_service.notice_board.web;
 
+import com.web_service.notice_board.config.auth.dto.SessionUser;
 import com.web_service.notice_board.domain.posts.PostsService;
 import com.web_service.notice_board.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,14 +9,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsService postsService;
-
+    private final HttpSession httpSession;
     @GetMapping("/")
     public String index(Model model){
+
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
