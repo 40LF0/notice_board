2c62c46494934ce8bf0228bff7fdfc84cd99ddcb
2019082851 <2019082851@hanyang.ac.kr>
2023-02-13 17:53:25 +0900

    SessionUser implement the "java.io.Serializable" interface.

--- src/main/java/com/web_service/notice_board/config/auth/dto/SessionUser.java
+++ src/main/java/com/web_service/notice_board/config/auth/dto/SessionUser.java
@@ -2,9 +2,10 @@ package com.web_service.notice_board.config.auth.dto;
 
 import com.web_service.notice_board.domain.user._User;
 import lombok.Getter;
+import java.io.Serializable;
 
 @Getter
-public class SessionUser {
+public class SessionUser implements Serializable {
     private String name;
     private String email;
     private String picture;
